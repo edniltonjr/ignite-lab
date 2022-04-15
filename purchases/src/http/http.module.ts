cee82a23548e0ea856967652b6bb0b53d703acaf
edniltonjr <jrjunior_cg@live.com>
2022-04-15 20:05:49 -0300

    Resolvers and Queries of Services

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -3,19 +3,31 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
+import { CustomersService } from 'src/services/customers.service';
+import { ProductsService } from 'src/services/product.service';
+import { PurchasesService } from 'src/services/purchases.service';
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from '../test/test.resolver';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        DatabaseModule,
-        GraphQLModule.forRoot({
-            driver: ApolloDriver,
-            autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
-        }),
-    ],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 
-    providers: [TestResolver],
+  providers: [
+    ProductsResolver,
+    ProductsService,
+    PurchasesResolver,
+    PurchasesService,
+    CustomersService,
+    CustomersResolver,
+  ],
 })
-export class HttpModule { }
+export class HttpModule {}
